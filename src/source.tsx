185b9e87a88123714c2177a7ef5d27ae2750bbc1
bnn16 <bogdan.nikolov4@Outlook.com>
2025-06-18 00:47:26 +0200

    feat: initial starting point of app

--- src/source.tsx
+++ src/source.tsx
@@ -1,16 +1,44 @@
-import { Box, render } from "ink";
+import { Box, render, useInput, useStdout } from "ink";
 import BigText from "ink-big-text";
 
 const AsciiApp = () => {
+    useInput((input, key) => {
+        if (key.ctrl && input === "c") {
+            process.exit(0);
+        }
+    });
+
+    const stdout = useStdout();
+
+    stdout.write("\x1b[?1049h");
+
     return (
-        <Box flexDirection="row">
-            <BigText text="gcm" colors={["#28a745", "#f85149"]} />
-
-            <BigText text="-" colors={["#f85149", "#f85149"]} />
-
-            <BigText text="vibed" colors={["#28a745", "#f85149"]} />
+        <Box
+            borderStyle="round"
+            flexDirection="column"
+            height="100vh"
+            width="100vw"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Box
+                flexDirection="row"
+                justifyContent="center"
+                alignItems="center"
+                flexWrap="wrap"
+                gap={1}
+            >
+                <BigText text="gcm" colors={["#28a745", "#f85149"]} />
+                <BigText text="vibed" colors={["#28a745", "#f85149"]} />
+            </Box>
         </Box>
     );
 };
 
-render(<AsciiApp />);
+console.clear();
+
+const app = render(<AsciiApp />);
+
+await app.waitUntilExit().then(() => {
+    console.log("App exited");
+});
